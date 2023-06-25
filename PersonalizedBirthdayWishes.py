7a474688d57c04fdb9b4187f8a59f253af7c370d
郭未璇 <guoweixuan@WeixuandeMacBook-Pro.local>
2023-06-25 18:19:02 +0100

    this is really the last time qk

--- PersonalizedBirthdayWishes.py
+++ PersonalizedBirthdayWishes.py
@@ -1,9 +1,14 @@
+print ("This is a birthdy card generator assistant. You need to enter some information for me.")
+
 recipients_name = input("what's the recipient's name? ")
 recipients_birth_year= input("what's the recipient's birth year? ")
 age=2023-int(recipients_birth_year)
 short_personalized_message=input("Add a personalized message: ")
 senders_name=input ("put the senders name: ")
 
+print()
+print("this is the generated message:")
+print()
 
 print(f"""{recipients_name},let's celebrate your {age} years of awesomeness! 
 Wishing you a day filled with joy and laughter as you turn {age}!
