a11ea5bf967687b0669c539d2f245da306b897aa
郭未璇 <guoweixuan@WeixuandeMacBook-Pro.local>
2023-06-25 17:38:55 +0100

    finishing the ass

--- PersonalizedBirthdayWishes.py
+++ PersonalizedBirthdayWishes.py
@@ -1,7 +1,13 @@
 recipients_name = "Hodei"
 age=2023-1987
-short_personalized_message="wish you being dumb for 100 years"
+short_personalized_message="wish you being dumb for 100 years!"
 senders_name="weixuan"
 
 
-print(f"{recipients_name},let's celebrate your {age} years of awesomeness!")
+print(f"""{recipients_name},let's celebrate your {age} years of awesomeness! 
+Wishing you a day filled with joy and laughter as you turn {age}!
+
+{short_personalized_message}
+
+With love and best wishes,
+{senders_name}""")
