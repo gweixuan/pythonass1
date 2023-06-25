2fd183cf696aad2bd7bf60ef9dee41b8a39bd04f
郭未璇 <guoweixuan@WeixuandeMacBook-Pro.local>
2023-06-25 17:28:24 +0100

    modified the message

--- PersonalizedBirthdayWishes.py
+++ PersonalizedBirthdayWishes.py
@@ -1,4 +1,7 @@
 recipients_name = "Hodei"
-age=2023-1800
+age=2023-1987
 short_personalized_message="wish you being dumb for 100 years"
 senders_name="weixuan"
+
+
+print(f"{recipients_name},let's celebrate your {age} years of awesomeness!")
